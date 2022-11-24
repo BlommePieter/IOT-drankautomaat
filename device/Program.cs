e2618ed6a8f6277a62eec0e922851190b3e57a80
Pieter <pieter.blomme@student.howest.be>
2022-11-24 12:13:30 +0100

    startfiles

---
+++ device/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using machineapp.Models;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+
+
+string connectionString = "";
+var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+
+#region Menu
+
+await Loop();
+async Task Loop()
+{
+    while (true)
+    {
+        Console.WriteLine("Maak uw keuze: ");
+        Console.WriteLine("1. Water");
+        Console.WriteLine("2. Cola");
+        Console.WriteLine("3. Fruitsap");
+        Console.WriteLine("4. Afsluiten");
+        string keuze = Console.ReadLine();
+        await ProcessChoice(keuze);
+    }
+}
+async Task ProcessChoice(string choise)
+{
+}
+
+#endregion
